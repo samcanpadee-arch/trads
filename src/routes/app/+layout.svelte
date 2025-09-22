eef1c59f3db825760f160ecde06227e4b325aead
samcanpadee-arch <samcanpadee@gmail.com>
2025-09-22 03:25:20 +0000

    wrap /app with TooltipProvider to fix SSR context error

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  // Use the wrapper that came with the template
+  import { TooltipProvider } from '$lib/components/ui/tooltip';
+</script>
+
+<TooltipProvider>
+  <slot />
+</TooltipProvider>
