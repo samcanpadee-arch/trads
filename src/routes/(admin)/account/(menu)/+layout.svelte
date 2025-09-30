d4f8b303ef35d839f897a998429c0e5d92fc93a3
samcanpadee-arch <samcanpadee@gmail.com>
2025-09-30 23:54:22 +0000

    fix(layout): restore +layout.svelte to main (revert li edit)

--- src/routes/(admin)/account/(menu)/+layout.svelte
+++ src/routes/(admin)/account/(menu)/+layout.svelte
@@ -110,7 +110,6 @@
           Billing
         </a>
       <li>
-      </li>
         <a href="/account/chat" class="gap-2">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <rect x="3" y="4" width="18" height="14" rx="2" ry="2"></rect>
