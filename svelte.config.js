bccffec74b9a8229efe68f41f9c6533248f5fd9f
samcanpadee-arch <samcanpadee@gmail.com>
2025-09-30 11:25:16 +0000

    build: simplify svelte.config.js (no vitePreprocess)

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
-  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter()
   }
