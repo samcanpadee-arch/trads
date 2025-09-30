b147eca7d5a91ad2e04fa53ec79ae6b1e68f0fa1
samcanpadee-arch <samcanpadee@gmail.com>
2025-09-30 11:22:28 +0000

    chore: use @sveltejs/adapter-vercel for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,11 @@
-import adapter from "@sveltejs/adapter-auto"
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
-    // allow up to 150kb of style to be inlined with the HTML
-    // Faster FCP (First Contentful Paint) by reducing the number of requests
-    inlineStyleThreshold: 150000,
-  },
   preprocess: vitePreprocess(),
-}
+  kit: {
+    adapter: adapter()
+  }
+};
 
-export default config
+export default config;
